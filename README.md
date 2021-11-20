45805e75cd79ed606e3cbfb8102c88009d582720
PedroSilva201 <13pedro.tome@gmail.com>
2021-11-20 15:52:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bv91ax
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bv91ax)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Seja bem vindo!</h1>
+      <p>Inicie sua carreira aqui :)</p>
+      <p>foram tempos de gloria em nossa cidade</p>
+    </div>
+  );
+}
